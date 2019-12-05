63171c583a82326b7a46b5bc899ff012d4d4bcb4
Edouard Rucquoy <erucquoy@gmail.com>
2019-12-05 22:51:10 +0100

    new repo for ssr vue 2.0

---
+++ src/components/SecondLevel.vue
@@ -0,0 +1,11 @@
+<template>
+	<ul class="footermenu">
+      <li><router-link exact to="/imprint">Imprint</router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+	name: 'SecondLevel'
+}
+</script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer-page">
+    <ul class="footermenu">
+      <li><router-link exact to="/contact">Contact</router-link></li>
+    </ul>
+    <ul class="footermenu">
+      <li><router-link exact to="/imprint">Imprint</router-link></li>
+    </ul>
+    <div>
+      <second-level />
+    </div>
+  </footer>
+</template>
+
+<script>
+import SecondLevel from './SecondLevel.vue'
+
+  export default {
+    name: 'TheFooter',
+    components: {
+      SecondLevel
+    }
+  }
+</script>
